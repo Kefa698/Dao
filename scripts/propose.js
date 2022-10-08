2ac340146f3a879f2a1e40ec49f679e4974ef1a5
Kefa698 <kefaombongi07@gmail.com>
2022-10-08 12:16:29 +0300

    propose.js

---
+++ scripts/propose.js
@@ -0,0 +1,44 @@
+const { ethers, network } = require("hardhat");
+const {
+  developmentChains,
+  FUNC,
+  NEW_STORE_VALUE,
+  proposalFile,
+  PROPOSAL_DESCRIPTION,
+  VOTING_DELAY,
+} = require("../helper-hardhat-config");
+const { moveBlocks } = require("../utils/move-blocks");
+const fs = require("fs");
+
+async function propose(args, functionToCall, proposalDescription) {
+  const governor = await ethers.getContract("GovernorContract");
+  const box = await ethers.getContract("Box");
+  const encodedFunctionCall = box.interface.encodeFunctionData(
+    functionToCall,
+    args
+  );
+  console.log(`Proposing ${functionToCall} on ${box.address} with ${args}`);
+  console.log(`Proposal Description:\n  ${proposalDescription}`);
+  const proposeTx = await governor.propose(
+    [box.address],
+    [0],
+    [encodedFunctionCall],
+    proposalDescription
+  );
+  const proposeReceipt = await proposeTx.wait(1);
+  // If working on a development chain, we will push forward till we get to the voting period.
+  if (developmentChains.includes(network.name)) {
+    await moveBlocks(VOTING_DELAY + 1);
+  }
+  const proposalId = proposeReceipt.events[0].args.proposalId;
+  let proposals = JSON.parse(fs.readFileSync(proposalFile, "utf8"));
+  proposals[network.config.chainId.toString()].push(proposalId.toString());
+  fs.writeFileSync(proposalFile, JSON.stringify(proposals));
+}
+
+propose([NEW_STORE_VALUE], FUNC, PROPOSAL_DESCRIPTION)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
