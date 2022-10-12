1ddaaaa1059904d88b7b557244b92a00ed1b5729
Kefa698 <kefaombongi07@gmail.com>
2022-10-12 15:28:14 +0300

    fixed

--- deploy/02-timelock-contact.js
+++ deploy/02-timelock-contact.js
@@ -1,36 +1,33 @@
-const { network } = require("hardhat");
+const { network } = require("hardhat")
 const {
-  developmentChains,
-  VERIFICATION_BLOCK_CONFIRMATIONS,
-  MIN_DELAY,
-} = require("../helper-hardhat-config");
-const { verify } = require("../utils/verify");
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    MIN_DELAY,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
-  const waitBlockConfirmations = developmentChains.includes(network.name)
-    ? 1
-    : VERIFICATION_BLOCK_CONFIRMATIONS;
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
 
-  log("----------------------------------------------------");
-  const arguments = [MIN_DELAY, [], []];
-  const Timelock = await deploy("Timelock", {
-    from: deployer,
-    args: arguments,
-    log: true,
-    waitConfirmations: waitBlockConfirmations,
-  });
+    log("----------------------------------------------------")
+    const arguments = [MIN_DELAY, [], []]
+    const Timelock = await deploy("Timelock", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
 
-  // Verify the deployment
-  if (
-    !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
-  ) {
-    log("Verifying...");
-    await verify(Timelock.address, arguments);
-  }
-  log("----------------------------------------------------");
-};
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(Timelock.address, arguments)
+    }
+    log("----------------------------------------------------")
+}
 
-module.exports.tags = ["all", "timelock"];
+module.exports.tags = ["all", "timelock"]
