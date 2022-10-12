53355b3c1e7ff1cd8f4b5545fc4bf18c3f7ef2cd
Kefa698 <kefaombongi07@gmail.com>
2022-10-12 15:28:52 +0300

    fixed

--- scripts/vote.js
+++ scripts/vote.js
@@ -1,38 +1,34 @@
-const fs = require("fs");
-const { network, ethers } = require("hardhat");
+const fs = require("fs")
+const { network, ethers } = require("hardhat")
 const {
-  developmentChains,
-  proposalFile,
-  VOTING_PERIOD,
-  reason,
-} = require("../helper-hardhat-config");
-const { moveBlocks } = require("../utils/move-blocks");
+    developmentChains,
+    proposalFile,
+    VOTING_PERIOD,
+    reason,
+} = require("../helper-hardhat-config")
+const { moveBlocks } = require("../utils/move-blocks")
 
-const index = 0;
+const index = 0
 
 async function vote(proposalIndex) {
-  const proposal = JSON.parse(fs.readFileSync(proposalFile, "utf8"));
-  const proposalId = proposal[network.config.chainId][proposalIndex];
-  //0=against,1=for,2=abstain
-  const voteWay = 1;
-  const governor = await ethers.getContract("GovernorContract");
-  
-  const voteTxResponse = await governor.castVoteWithReason(
-    proposalId,
-    voteWay,
-    reason
-  );
-  await voteTxResponse.wait(1);
+    const proposal = JSON.parse(fs.readFileSync(proposalFile, "utf8"))
+    const proposalId = proposal[network.config.chainId][proposalIndex]
+    //0=against,1=for,2=abstain
+    const voteWay = 1
+    const governor = await ethers.getContract("GovernorContract")
 
-  if (developmentChains.includes(network.name)) {
-    await moveBlocks(VOTING_PERIOD + 1);
-  }
-  console.log("voted ready to go");
+    const voteTxResponse = await governor.castVoteWithReason(proposalId, voteWay, reason)
+    await voteTxResponse.wait(1)
+
+    if (developmentChains.includes(network.name)) {
+        await moveBlocks(VOTING_PERIOD + 1)
+    }
+    console.log("voted ready to go")
 }
 
 vote(index)
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
